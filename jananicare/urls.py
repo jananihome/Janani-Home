34006e14ad388473cabb9aa6a0b65d5a91d18795
Maciej Lorenc <lorencmaciek@gmail.com>
2017-05-30 23:47:26 +0200

    Initial project structure with development settings

---
+++ jananicare/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+# Static helper function only for development!
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('educationalneed.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
