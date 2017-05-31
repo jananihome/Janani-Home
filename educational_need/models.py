f27bf2351b64b81d6c1ea2f11ead376937300666
Maciej Lorenc <lorencmaciek@gmail.com>
2017-05-31 23:12:35 +0200

    Removed title field

--- educational_need/models.py
+++ educational_need/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class EducationalNeed(models.Model):
     user = models.ForeignKey('auth.User')  # Need to replace with our custom user
-    title = models.CharField(max_length=200)
     permanent_address = models.TextField()
     current_address = models.TextField()
     college_school_address = models.TextField(blank=True, null=True)
@@ -16,4 +15,4 @@ class EducationalNeed(models.Model):
     communication_mode = models.CharField(max_length=200)
 
     def _str_(self):
-        return self.title
+        return 'Educational Need ' + self.pk
