e27a6c16d872e0b322591fa4ee6ee3ba9b8f2799
Maciej Lorenc <lorencmaciek@gmail.com>
2017-06-01 21:15:06 +0200

    Implemented html template for educational need results view

--- educational_need/views.py
+++ educational_need/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import EducationalNeed
 
-def index(request):
-    return HttpResponse('It works!')
+
+def educational_need(request):
+
+    educational_needs = EducationalNeed.objects.all()
+    template = 'educational_need/educational_need.html'
+    context = {'educational_needs': educational_needs}
+
+    return render(request, template, context)
