bc08bfcfb7e71939ffece7793239e6b75e44e935
Maciej Lorenc <lorencmaciek@gmail.com>
2017-06-12 22:35:41 +0200

    Added url pattern for educational need form.

--- educational_need/urls.py
+++ educational_need/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
+	url(r'^add-educational-need/$', views.add_educational_need, name='add_educational_need'),
 ]
