f0978fcc01538a1cdcb1cb09c53b105345a29155
Maciej Lorenc <lorencmaciek@gmail.com>
2017-06-12 23:39:14 +0200

    Connected EducationalNeedForm with add new need view and form template.

--- educational_need/views.py
+++ educational_need/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Model imports
 from .models import EducationalNeed
+# Form imports
+from .forms import EducationalNeedForm
 
 
 def list_view(request):
@@ -13,5 +15,29 @@ def list_view(request):
 
 
 def add_educational_need(request):
-	template = 'educational_need/form_view.html'
-	return render(request, template)
+    """Returns a view for adding EducationalNeed objects and handles POST
+    requests submitted through the form."""
+    
+    # If request method is POST, process form data.
+    if request.method == 'POST':
+        # Collect data from the form.
+        form = EducationalNeedForm(request.POST, request.FILES)
+        # If form data is valid, process data and save.
+        if form.is_valid():
+            # False save to variable first, because we need to modify data
+            # before commiting it to db.
+            eductional_need = form.save(commit=False)
+            # Set user to currently logged in user.
+            eductional_need.user = request.user
+            # Save model data in db.
+            eductional_need.save()
+            # Redirect after submitting the form
+            return redirect('list_view')
+    # If request is not POST, create empty form.
+    else:
+        form = EducationalNeedForm()
+        
+    # Create context dictionary which can be accessed in template
+    context = {'form': form}
+    template = 'educational_need/form_view.html'
+    return render(request, template, context)
