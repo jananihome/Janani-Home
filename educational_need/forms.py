3e404120375cb08bbe209ba1b2f26b283cf4d122
Maciej Lorenc <lorencmaciek@gmail.com>
2017-06-12 23:01:44 +0200

    Defined educational need form.

---
+++ educational_need/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+# Models imports
+from .models import EducationalNeed
+
+class EducationalNeedForm(forms.ModelForm):
+	"""Defines a front-end form for EducationalNeed model."""
+    class Meta:
+        model = EducationalNeed
+        fields = (
+			'permanent_address',
+			'current_address',
+			'college_school_address',
+			'college_school_contact_details',
+			'status',
+			'amount_required',
+			'documents',
+			'requirement_description',
+			'communication_mode',)
