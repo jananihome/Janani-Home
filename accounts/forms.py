a3e5cef99015f19eea075b338f99064fcc424bee
Maciej Lorenc <lorencmaciek@gmail.com>
2017-07-15 18:31:16 +0200

    Made username case insensitive.

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,20 +2,29 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from .models import Profile
-from educational_need.models import EducationalNeed
-        
+
+
 class SignupForm(UserCreationForm):
+
     email = forms.EmailField(max_length=200)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    def clean(self):
+        cleaned_data = super(SignupForm, self).clean()
+        username = cleaned_data.get('username')
+        if username and User.objects.filter(username__iexact=username).exists():
+            self.add_error('username', 'A user with that username already exists.')
+        return cleaned_data
+
 
 class UserForm(forms.ModelForm):
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email')
 
 
 class ProfileForm(forms.ModelForm):
