76d4ccc098e21df0bf8c8173d9a4f6f9b5410bca
Maciej Lorenc <lorencmaciek@gmail.com>
2017-07-15 19:01:27 +0200

    Made email unique.

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,16 +15,31 @@ class SignupForm(UserCreationForm):
     def clean(self):
         cleaned_data = super(SignupForm, self).clean()
         username = cleaned_data.get('username')
+        email = cleaned_data.get('email')
         if username and User.objects.filter(username__iexact=username).exists():
             self.add_error('username', 'A user with that username already exists.')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            self.add_error('email', 'A user with that email already exists.')
         return cleaned_data
 
 
 class UserForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].disabled = True
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        email = cleaned_data.get('email')
+        username = cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            self.add_error('email', 'A user with that email already exists.')
+        return cleaned_data
 
 
 class ProfileForm(forms.ModelForm):
