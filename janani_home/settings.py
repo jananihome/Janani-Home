215d1aaf1426a78e37b9241a9f0741c772fef3a0
Maciej Lorenc <lorencmaciek@gmail.com>
2018-04-03 21:45:03 +0200

    Set timeout for activation tokens to 4 days:

--- janani_home/settings.py
+++ janani_home/settings.py
@@ -7,6 +7,7 @@ from decouple import config, Csv
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = config('DEBUG', default=False, cast=bool)
+PASSWORD_RESET_TIMEOUT_DAYS = 4
 SECRET_KEY = config('SECRET_KEY')
 SERVER_EMAIL = config('SERVER_EMAIL', default='root@localhost')
 SESSION_COOKIE_AGE = 60 * 30
